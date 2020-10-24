a1946e3e4922b7387103fb3af8f51e36856a2cf0
Vineeth Pulipati <mynameisvineeth@gmail.com>
2020-10-24 14:39:06 +0530

    Update Shared_Ptr.cpp

--- Shared_Ptr.cpp
+++ Shared_Ptr.cpp
@@ -81,5 +81,6 @@ int main() {
 sp3.reset(new double(420.69));
 std::cout << "sp3 value " <<*sp3<<"\n";
 std::cout << "sp3 sole owner? " << std::boolalpha << sp3.unique() << '\n';
-
+sp2.reset(new double(3.1415), Deleter<double>());
+std::cout << "sp2 shared # " << sp2.use_count() << '\n';
 }
